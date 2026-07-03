9345cfd44ebd3264418317346f2a4dbd53182874
giovanatoledooliveira2012-lang <giovana.toledooliveira2012@gmail.com>
2026-07-03 16:23:38 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  base: '/KRB-final/',
+
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
